d9af293b3607d3e70edefdcaad5101e8fd4b64b3
MacBook <macbook@MacBook-Air-MacBook.local>
2023-07-05 09:46:04 +0200

    v3

--- next.config.js
+++ next.config.js
@@ -6,7 +6,6 @@ module.exports = {
     ignoreDuringBuilds: true,
   },
   reactStrictMode: true,
-
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
